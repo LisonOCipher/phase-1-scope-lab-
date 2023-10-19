276ed555907b5c1a8231a05632300df1bd5a269b
Danson Lino <danson.lino@student.moringaschool.com>
2023-10-19 16:39:08 +0300

    let's  commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// declare customerName
+let customerName = "bob";
+// modifying customerName
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+// setting bestCustomer
+let bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+// overwrite
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+// Unsuccessful reassignment
+const changeLeastFavoriteCustomer = "initial"
+function reassignChangeLeastFavoriteCustomer(){
+    changeLeastFavoriteCustomer = "changed";
+}
